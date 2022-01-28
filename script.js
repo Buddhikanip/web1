f47887c9821ac5a19b7caa5133f0d5f6bfef9fdd
Buddhikanip <98378758+Buddhikanip@users.noreply.github.com>
2022-01-28 08:25:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
-function pageReload(){
-  var x =window.location = "index.html";
-  x.location.reload();
+
+var a=1;
+
+function m(){
+  var x=document.getElementById("summary");
+  x.className="free";
 }
-function pagereloadstart(){
-  setInterval(pageReload,3000);
+
+function n(){
+  var x=document.getElementById("scorecard");
+  x.className="active";
+}
+function o(){
+  var x=document.getElementById("scorecard");
+  x.className="free";
+}
+
+function p(){
+  var x=document.getElementById("summary");
+  x.className="active";
+}
+
+
+
+function s(){
+  var x=document.getElementById("bbulls");
+  x.className="free";
+}
+
+function q(){
+  var x=document.getElementById("bbulls");
+  x.className="active";
+}
+function r(){
+  var x=document.getElementById("swar");
+  x.className="free";
+}
+
+function t(){
+  var x=document.getElementById("swar");
+  x.className="active";
+}
+
+var x;
+
+function x(){
+  x=document.getElementById("bullsTotal");
+  x.innerHTML="10";
 }
